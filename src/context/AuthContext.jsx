c6125d60a1e628d779ce9d3c43614723a1cbeeb6
kritikshya12 <kritikshya.a@gmail.com>
2025-08-20 23:30:09 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [loading, setLoading] = useState(true);
 
-  const API_BASE_URL =  'import.meta.env.VITE_API_URL';
+  const API_BASE_URL =  "https://flatmaintenance-f7vq.onrender.com/api";
 
   // Check if user is authenticated on app load
   useEffect(() => {
